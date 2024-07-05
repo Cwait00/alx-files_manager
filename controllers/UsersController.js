1d6bdce6ce48a7507592ba4271f711b6f6cae357
cwait00 <cwaitanobongoza@gmail.com>
2024-07-05 08:14:47 -0700

    Redis utils

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,34 +1,29 @@
-const crypto = require('crypto');
+const sha1 = require('sha1');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+    static async postNew(req, res) {
+        const { email, password } = req.body;
 
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const existingUser = await dbClient.db.collection('users').findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        const hashedPassword = sha1(password);
+        const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+
+        return res.status(201).json({ id: result.insertedId, email });
     }
-
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-
-    const userExists = await dbClient.db.collection('users').findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
-
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    const result = await dbClient.db.collection('users').insertOne(newUser);
-
-    return res.status(201).json({ id: result.insertedId, email });
-  }
 }
 
 module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
 
-// Existing routes
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+const router = express.Router();
 
-// New endpoint for creating a user
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
-// Endpoint for uploading files
-router.post('/files', FilesController.postUpload);
 
 module.exports = router;
