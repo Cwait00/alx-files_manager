d0e12e12723f02fc637fafa1ddcf099371d7baa8
cwait00 <cwaitanobongoza@gmail.com>
2024-07-05 03:02:07 -0700

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,46 +1,29 @@
-const Bull = require('bull');
-const { v4: uuidv4 } = require('uuid');
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
-const userQueue = new Bull('userQueue');
-
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+    static async postNew(req, res) {
+        const { email, password } = req.body;
 
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const existingUser = await dbClient.db.collection('users').findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        const hashedPassword = sha1(password);
+        const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+
+        return res.status(201).json({ id: result.insertedId, email });
     }
-
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-
-    const usersCollection = dbClient.db.collection('users');
-    const existingUser = await usersCollection.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
-
-    const hashedPassword = sha1(password);
-    const user = {
-      email,
-      password: hashedPassword,
-    };
-
-    const result = await usersCollection.insertOne(user);
-    const userId = result.insertedId;
-
-    userQueue.add({ userId });
-
-    return res.status(201).json({
-      id: userId,
-      email,
-    });
-  }
 }
 
 module.exports = UsersController;
