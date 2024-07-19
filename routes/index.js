3e2e753239896925837133881f968372d71bbe0d
cwait00 <cwaitanobongoza@gmail.com>
2024-07-19 05:42:43 -0700

    fix: correct token handling for GET /users/me and resolve ESLint issues

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,6 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-router.get('/files/:id/data', FilesController.getFile); // New endpoint
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
